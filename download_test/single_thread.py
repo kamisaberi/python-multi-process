175cd5e47bde88e3f839c88581fc9ffd0bd692ae
= <=>
2024-03-30 03:14:56 +0330

    initial release

--- download_test/single_thread.py
+++ download_test/single_thread.py
@@ -2,10 +2,16 @@ import requests
 import random
 import string
 
-with  (links := open('links.txt', "rt", encoding="utf8")):
+with  open('links.txt', "rt") as f1:
+    links = f1.read().split("\n")
     for link in links:
         ext = link.strip().split("/")[-1].strip().split("?")[0].strip().split(".")[-1]
+        name = link.strip().split("/")[-1].strip().split("?")[0].strip().split(".")[0]
+        file_name = name + "." + ext
+        print(file_name)
         content = requests.get(link).content
-        name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
-        with open("files/single/" + name + "." + ext, "wb") as file:
+        # name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
+        with open(n := "files/single/" + file_name, "wb") as file:
             file.write(content)
+            print(n)
+        print(link)
