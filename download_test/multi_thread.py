011b481e0959a4168d6a39eab60c342241fde975
= <=>
2024-03-30 03:28:05 +0330

    initial release

---
+++ download_test/multi_thread.py
@@ -0,0 +1,31 @@
+import os
+import threading
+
+import requests
+
+if not os.path.exists("files/"):
+    os.mkdir("files")
+if not os.path.exists("files/multi/"):
+    os.mkdir("files/multi/")
+
+
+def download_image(link: str):
+    ext = link.strip().split("/")[-1].strip().split("?")[0].strip().split(".")[-1]
+    name = link.strip().split("/")[-1].strip().split("?")[0].strip().split(".")[0]
+    content = requests.get(link.strip()).content
+    # name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
+    with open(n := "files/single/" + name + "." + ext, "wb") as file:
+        file.write(content)
+        print(n)
+    print(link)
+
+
+with  open('links.txt', "rt") as links:
+    threads = []
+    for link in links:
+        th = threading.Thread(target=download_image, args=(link,))
+        th.start()
+        th.join()
+        threads.append(th)
+    for thread in threads:
+        thread.join()
--- download_test/single_thread.py
+++ download_test/single_thread.py
@@ -1,17 +1,20 @@
 import requests
 import random
 import string
+import os
 
-with  open('links.txt', "rt") as f1:
-    links = f1.read().split("\n")
+if not os.path.exists("files/"):
+    os.mkdir("files")
+if not os.path.exists("files/single/"):
+    os.mkdir("files/single/")
+
+with  open('links.txt', "rt") as links:
     for link in links:
         ext = link.strip().split("/")[-1].strip().split("?")[0].strip().split(".")[-1]
         name = link.strip().split("/")[-1].strip().split("?")[0].strip().split(".")[0]
-        file_name = name + "." + ext
-        print(file_name)
-        content = requests.get(link).content
+        content = requests.get(link.strip()).content
         # name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
-        with open(n := "files/single/" + file_name, "wb") as file:
+        with open(n := "files/single/" + name + "." + ext, "wb") as file:
             file.write(content)
             print(n)
         print(link)
